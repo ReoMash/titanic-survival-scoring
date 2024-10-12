b1e92590e325e81ee00f833d106afb3b0b251509
ReoMash <145448369+ReoMash@users.noreply.github.com>
2024-10-13 00:54:08 +0900

    fixup! fixup! fixup! deploy reset

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
   output: 'export',
   distDir: 'docs',
+  basePath: '/titanic-survival-scoring',
+  assetPrefix: '/titanic-survival-scoring',
 };
 
 export default nextConfig;
