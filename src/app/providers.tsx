1411dccdf5115f0430ad4e4884baae3ba40dede1
ReoMash <145448369+ReoMash@users.noreply.github.com>
2024-10-13 00:43:51 +0900

    deploy reset

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/react';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ChakraProvider>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <CSSReset />
+      {children}
+    </ChakraProvider>
+  );
 }
